265c21d33dc4e934045fda2e0e2ae33aeee070d8
todor1 <todorowa_el@abv.bg>
2024-08-17 21:24:23 +0300

    udemy+ws intro

--- django-basic/mysite/views.py
+++ django-basic/mysite/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from employees.models import Employee
 
 # def home(request):
 #     return HttpResponse("<h3>Hello, Django!</h3>")
 
 
 def home(request):
-    return render(request=request, template_name="home.html")
+    employees = Employee.objects.all()
+    context = {"employees": employees}
+    return render(request=request, template_name="home.html", context=context)
+    # return render(request=request, template_name="home.html")
