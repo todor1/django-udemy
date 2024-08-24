7b4c1b98eaac46f2bf8ff0326f29852cd3672948
todor1 <todorowa_el@abv.bg>
2024-08-24 17:56:02 +0300

    django basics pt1 end

--- django-basic/employees/views.py
+++ django-basic/employees/views.py
@@ -1,9 +1,10 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Employee
-from django.http import Http404, HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 def employee_detail(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
-    return HttpResponse(employee)
+    # return HttpResponse(employee)
+    return render(request, "employee_detail.html", {"employee": employee})
