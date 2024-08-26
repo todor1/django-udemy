40007a87c83596ed75847aaa9385f8bfab24ad3d
todor1 <todorowa_el@abv.bg>
2024-08-26 12:48:27 +0300

    django basic todo finished

--- django-todo/todo/urls.py
+++ django-todo/todo/urls.py
@@ -3,6 +3,15 @@ from . import views
 
 
 urlpatterns = [
+    # Add task
     path("addTask/", views.addTask, name="addTask"),
+    # Mark task as done
     path("mark_as_done/<int:pk>", views.mark_as_done, name="mark_as_done"),
+    # Mark task as undone
+    path("mark_as_undone/<int:pk>", views.mark_as_undone, name="mark_as_undone"),
+    # Edit task
+    path("edit_task/<int:pk>", views.edit_task, name="edit_task"),
+
+    # Delete task
+     path("delete_task/<int:pk>", views.delete_task, name="delete_task"),
 ]
--- django-todo/todo/views.py
+++ django-todo/todo/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 import re
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse    
@@ -18,4 +19,31 @@ def mark_as_done(request, pk):
     # return HttpResponse(task)
     task.is_completed = True
     task.save()
+    return redirect("home")
+
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect("home")
+
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        new_task = request.POST["task"]
+        get_task.task = new_task
+        get_task.save()
+        return redirect("home")
+    else:
+        context = {
+            "get_task": get_task
+        }
+        return render(request, "edit_task.html", context)
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect("home")
