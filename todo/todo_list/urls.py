2d1166c87e63c7f8bcb132eef0459fff1f0ad838
todor1 <todorowa_el@abv.bg>
2024-08-31 20:59:09 +0300

    todo2 started

--- todo/todo_list/urls.py
+++ todo/todo_list/urls.py
@@ -1,7 +1,7 @@
-
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("", v),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
 ]
