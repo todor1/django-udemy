f10617170aebc27415e3b99a5a452b5e0ac664d1
todor1 <todorowa_el@abv.bg>
2024-09-03 17:18:37 +0300

    stock format api results

--- django-stock/quotes/views.py
+++ django-stock/quotes/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html", {})
+    import os
+    import json
+    import requests
+
+    ticker = "AAPL"
+    api_key = os.getenv("POLYGON_API_KEY")
+    url = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/2023-01-09/2023-02-10?adjusted=true&sort=asc&apiKey={api_key}"
+
+    api_request = requests.get(url)
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, "home.html", {"api": api})
 
 
 def about(request):
