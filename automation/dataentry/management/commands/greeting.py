94ee7bf95e15be530206408edcedeb8908a4c6c0
todor1 <todorowa_el@abv.bg>
2024-09-09 12:01:20 +0300

    resources added

--- automation/dataentry/management/commands/greeting.py
+++ automation/dataentry/management/commands/greeting.py
@@ -7,7 +7,8 @@ class Command(BaseCommand):
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument("name", type=str, help="User name")
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
         name = kwargs["name"]
         greeting = f"Hi, {name}. Good morning!"
-        self.stdout.write(greeting)
+        # self.stdout.write(greeting)
+        self.stdout.write(self.style.SUCCESS(greeting))
