87a6f0a52188e48ca3df7d0ab96f5093b348e620
todor1 <todorowa_el@abv.bg>
2024-10-28 21:08:55 +0200

    djmc ch9 login

--- masterclass/mysite/users/views.py
+++ masterclass/mysite/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+from .forms import RegisterForm
+from django.contrib.auth import logout
+
+# from django.contrib.auth.forms import UserCreationForm
 
 # Django offers authentication views & html template out of the box.
 # We can use these views to create a login page.
@@ -8,14 +12,26 @@ from django.contrib import messages
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        ## form = UserCreationForm(request.POST)
+        ## extended form containing email field
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(
-                request, f"Welcome, {username}. Your account has been created."
+                # request, f"Welcome, {username}. Your account has been created."
+                request,
+                f"Welcome, {username}!",
             )
-            return redirect("food:index")
+            # return redirect("food:index")
+            return redirect("login")
     else:
-        form = UserCreationForm()
+        ## form = UserCreationForm()
+        ## extended form containing email field
+        form = RegisterForm()
     return render(request, "users/register.html", context={"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/logout.html")
