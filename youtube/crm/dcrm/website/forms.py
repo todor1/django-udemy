f715bca1765d2705b8cb96dc3bf80312022a586b
todor1 <todorowa_el@abv.bg>
2024-11-03 18:10:00 +0200

    yt crm final project

--- youtube/crm/dcrm/website/forms.py
+++ youtube/crm/dcrm/website/forms.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-
-# from .models import Record
+from .models import Record
 
 
 class SignUpForm(UserCreationForm):
@@ -61,3 +60,76 @@ class SignUpForm(UserCreationForm):
         self.fields["password2"].help_text = (
             '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
         )
+
+
+# create add record form
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "First Name", "class": "form-control"}
+        ),
+        label="",
+    )
+    last_name = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Last Name", "class": "form-control"}
+        ),
+        label="",
+    )
+    email = forms.EmailField(
+        max_length=100,
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Email", "class": "form-control"}
+        ),
+        label="",
+    )
+    phone = forms.CharField(
+        max_length=15,
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Phone", "class": "form-control"}
+        ),
+        label="",
+    )
+    address = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Address", "class": "form-control"}
+        ),
+        label="",
+    )
+    city = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "City", "class": "form-control"}
+        ),
+        label="",
+    )
+    state = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "State", "class": "form-control"}
+        ),
+        label="",
+    )
+    zip_code = forms.CharField(
+        max_length=10,
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Zip Code", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
+        # fields = "__all__
--- youtube/crm/dcrm/website/views.py
+++ youtube/crm/dcrm/website/views.py
@@ -1,12 +1,15 @@
+from math import e
 import re
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
     # check if the user is logged in
     if request.method == "POST":
         # get the user's input (name property of the input field)
@@ -27,7 +30,7 @@ def home(request):
             return redirect("home")
     else:
         # if the user is not posting data, return the home page
-        return render(request, "home.html", {})
+        return render(request, "home.html", {"records": records})
 
 
 ###########################################################################
@@ -64,3 +67,57 @@ def register_user(request):
 
 
 ###########################################################################
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up records by primary key
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {"customer_record": customer_record})
+    else:
+        messages.error(request, "You need to be logged in to view this page.")
+        return redirect("home")
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up records by primary key
+        record_to_delete = Record.objects.get(id=pk)
+        if record_to_delete:
+            record_to_delete.delete()
+            messages.success(request, "Record deleted.")
+            return redirect("home")
+    else:
+        messages.error(request, "You need to be logged in to delete a record.")
+        return redirect("home")
+
+
+def add_record(request):
+    # check if the user os posting daata, if not return a web page
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record added.")
+                return redirect("home")
+        return render(request, "add_record.html", context={"form": form})
+    else:
+        messages.error(request, "You need to be logged in to add a record.")
+        return redirect("home")
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        # populate the form with the current record: instance=current_record
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record updated.")
+            return redirect("home")
+        else:
+            return render(request, "update_record.html", context={"form": form})
+    else:
+        messages.error(request, "You need to be logged in to update a record.")
+        return redirect("home")
